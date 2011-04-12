ceb9b31418be66467750a33038f1caf8154eeed2
Dan Kalotay <d.kalotay@gmail.com>
2011-04-12 13:57:51 +0100

    probably solved problem 31, running very slowly though

--- 31/coins.hs
+++ 31/coins.hs
@@ -1,13 +1,4 @@
-coin_count :: Integer -> [Integer] -> [Integer]
-coin_count _ [] = []
-coin_count 0 _ = []
-coin_count i (x:xs)
-        | i >= x = (++) [x] (coin_count ((-) i x) (x:xs))
-        | otherwise = coin_count i xs
-
-net_coin_count :: Integer -> [Integer] -> [[Integer]]
-net_coin_count j y
-        | j > z = (++) [z:zs] (net_coin_count (j - z) (z:zs))
-        | otherwise = (++) [z:zs] (net_coin_count j zs)
-        where (z:zs) = coin_count j y
-              
+sum_count :: [Integer] -> Integer -> Integer
+sum_count _ 0 = 1
+sum_count poss amount = 
+        sum [sum_count poss (amount - x) | x <- poss, x <= amount]
