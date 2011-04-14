f289907bc0237a0487fc33eb81c46dfb93fa9b2f
Dan Kalotay <d.kalotay@gmail.com>
2011-04-14 18:12:48 +0100

    solved problem 31

--- 31/coins.hs
+++ 31/coins.hs
@@ -1,4 +1,7 @@
 sum_count :: [Integer] -> Integer -> Integer
 sum_count _ 0 = 1
-sum_count poss amount = 
-        sum [sum_count poss (amount - x) | x <- poss, x <= amount]
+sum_count [] _ = 0
+sum_count [1] _ = 1
+sum_count (x:xs) amount
+        | x > amount = sum_count xs amount
+        | otherwise = sum_count (x:xs) (amount - x) + sum_count xs amount 
