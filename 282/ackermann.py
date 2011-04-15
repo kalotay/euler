ff4f0286c97b55e60f0a8b92aa1f077e7ba5e684
Dan Kalotay <d.kalotay@gmail.com>
2011-04-15 16:02:30 +0100

    python attempt with memoization: recursion depth exceeded

---
+++ 282/ackermann.py
@@ -0,0 +1,10 @@
+import memoize as mem
+
+@mem.memoized
+def ackermann(m, n):
+    if m == 0:
+        return n + 1
+    elif n == 0:
+        return ackermann(m - 1, 1)
+    else:
+        return ackermann(m - 1, ackermann(m, n - 1))
---
+++ 282/memoize.py
@@ -0,0 +1,20 @@
+class memoized(object):
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+            return self.func(*args)
+
+
+    def __get__(self, obj, objtype):
+        return functools.partial(self.__call__, obj)
+
